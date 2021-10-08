30d8573eab9d4d084fdd497e5e52575c75ae9416
Kidus-dev <85897849+Kidus-dev@users.noreply.github.com>
2021-10-08 13:24:17 -0700

    note count

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import './App.css';
 import Notes from './components/Notes/notes';
 import NavBar from './components/NavBar/NavBar';
 import React from 'react';
+import axios from 'axios';
 function App() {
-	const [notes, setNotes] = React.useState([
-		{ id: Math.random(), title: 'Something', content: 'Something content' },
-	]);
+	const [notes, setNotes] = React.useState([]);
+	const [noteCount, setNoteCount] = React.useState(1);
 	const [searchNotesInput, setSearchNotesInput] = React.useState('');
 	const onAdd = () => {
 		const newNotes = [...notes];
 		newNotes.push({
 			id: Math.random(),
 			title: 'title here',
-			content: 'content here',
+			body: 'content here',
 		});
 		setNotes(newNotes);
 		//  setNotes([...notes, { title: '', content: '' }]);
@@ -26,7 +26,7 @@ function App() {
 	const onContentChange = (event, index) => {
 		const newNotes = notes.slice();
 		const newNote = newNotes[index];
-		newNotes.splice(index, 1, { ...newNote, content: event.target.value });
+		newNotes.splice(index, 1, { ...newNote, body: event.target.value });
 		setNotes(newNotes);
 	};
 	const onDelete = (id) => {
@@ -38,27 +38,47 @@ function App() {
 	const handleSearchNotesInput = (e) => {
 		setSearchNotesInput(e.target.value);
 	};
+	const handleNoteCountChange = (e) => {
+		setNoteCount(e.target.value);
+	};
+	React.useEffect(() => {
+		const func = async () => {
+			if (noteCount > 0) {
+				let res = await axios.get(
+					`https://jsonplaceholder.typicode.com/posts/`
+				);
+
+				const newNotes = Array.from(notes);
+				newNotes.push({
+					title: res.data[noteCount].title,
+					body: res.data[noteCount].body,
+				});
+				setNotes(newNotes);
+			}
+		};
+		func();
+	}, [noteCount]);
+
 	return (
 		<div className='app__container'>
 			<NavBar count={notes.length} />
+			<input
+				value={noteCount}
+				onChange={handleNoteCountChange}
+				className='app__note-search'
+			/>
 			<input
 				className='app__note-search'
 				placeholder='Search Notes'
 				value={searchNotesInput}
 				onChange={handleSearchNotesInput}
 			/>
+
 			<button className='btn__add-note' onClick={() => onAdd()}>
 				Add Note
 			</button>
 			<Notes
-				notes={notes
-					.filter(
-						(note) =>
-							note.title
-								.toLowerCase()
-								.indexOf(searchNotesInput.toLowerCase()) !== -1
-					)
-					.map((note) => note)}
+				notes={notes}
 				handleContentChange={onContentChange}
 				handleTitleChange={onTitleChange}
 				handleDeleteNote={onDelete}
--- src/components/Notes/notes.js
+++ src/components/Notes/notes.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import Note from '../Note/note';
-import './notes.style.css'
+import './notes.style.css';
 export default function Notes(props) {
 	const [allNotes, setAllNotes] = React.useState([]);
+	const [noteCount, setNoteCount] = React.useState(1);
 
 	return (
-		<div className='notes__container'>
-			{props.notes.map((note, i) => (
-				<Note
-					key={i}
-					handleContentChange={props.handleContentChange}
-					handleTitleChange={props.handleTitleChange}
-					handleDeleteNote={props.handleDeleteNote}
-					title={note.title}
-					index={i}
-					content={note.content}
-					id={note.id}
-				/>
-			))}
+		<div>
+			<div className='notes__container'>
+				{props.notes.map((note, i) => (
+					<Note
+						key={i}
+						handleContentChange={props.handleContentChange}
+						handleTitleChange={props.handleTitleChange}
+						handleDeleteNote={props.handleDeleteNote}
+						title={note.title}
+						index={i}
+						content={note.body}
+						id={note.id}
+					/>
+				))}
+			</div>
 		</div>
 	);
 }
